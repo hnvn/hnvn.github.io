78677cd0b2d6a0bb9aac4868683c7ecb4038bc1d
hunghd <hunghd.yb@gmail.com>
2021-10-19 01:20:57 +0700

    first commit

---
+++ precache-manifest.aa23a5b675a83e1df97e77feb53cadd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00fbb9f0b9049300c1e6bf23316d4b66",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "3c2e20d169f522f8e596",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "d74018fed2872d5117f4",
+    "url": "/home/static/js/2.11e38c3f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.11e38c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2e20d169f522f8e596",
+    "url": "/home/static/js/main.a4325b9a.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "5b3adaa7aae2c300113ef69f896d2985",
+    "url": "/home/static/media/hunghd.5b3adaa7.jpg"
+  },
+  {
+    "revision": "b155118612bec9bc1e0354e8d278acc4",
+    "url": "/home/static/media/resume.b1551186.pdf"
+  }
+]);
