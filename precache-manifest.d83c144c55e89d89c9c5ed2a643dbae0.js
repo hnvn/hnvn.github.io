b3f66a49447ce146d956d60c94a216c51fd920c5
hunghd <hunghd.yb@gmail.com>
2025-07-23 15:56:08 +0700

    Updates

---
+++ precache-manifest.d83c144c55e89d89c9c5ed2a643dbae0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9af1d0ceb218ccf78409fc741e75cd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c55892b911bec4a55c44",
+    "url": "/static/css/main.0329fe02.chunk.css"
+  },
+  {
+    "revision": "d74018fed2872d5117f4",
+    "url": "/static/js/2.11e38c3f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.11e38c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c55892b911bec4a55c44",
+    "url": "/static/js/main.44dd76ac.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "5b3adaa7aae2c300113ef69f896d2985",
+    "url": "/static/media/hunghd.5b3adaa7.jpg"
+  },
+  {
+    "revision": "03eefdbba41bb9187f1f45e556d0cf17",
+    "url": "/static/media/resume.03eefdbb.pdf"
+  }
+]);
